b1dc2fad64f2398b5e83bf0fe62e0f4f2986fe9a
NMaslenko <nvmaslenko@gmail.com>
2020-04-15 01:08:16 +0500

    -- change this file to functions and basefile

--- sub/shoes.py
+++ sub/shoes.py
@@ -1,71 +1,71 @@
-class Shoes:
-	Brands = {'Adidas': ('Gazelle', 'Hamburg', 'Spezial', 'Samba', 'EQT', 'ZX 8000', 'Yeezy 500'), 'Fila': ('Disruptor II', 'Sandals')}
+#class Shoes:
+	#Brands = {'Adidas': ('Gazelle', 'Hamburg', 'Spezial', 'Samba', 'EQT', 'ZX 8000', 'Yeezy 500'), 'Fila': ('Disruptor II', 'Sandals')}
 
-	Colors = {
-		'Gazelle':
-			[('PINK', '2190', 'https://sun9-67.userapi.com/c205724/v205724730/ded27/T1DCjpehweM.jpg'),
-			 ('BLACK', '2190', 'https://sun9-9.userapi.com/c205724/v205724730/ded4e/yxSS1YH3Dkw.jpg'),
-			 ('SKY BLUE', '2190', 'https://sun9-48.userapi.com/c205724/v205724613/e18d3/TKPTlBNQ39A.jpg'),
-			 ('GRAY BLACK', '2190', 'https://sun9-20.userapi.com/c205724/v205724613/e18ca/lBEx0-ob0Vs.jpg'),
-			 ('BLUE', '2190', 'https://sun9-20.userapi.com/c205724/v205724613/e1902/jwWux3r32YM.jpg'),
-			 ('GREEN', '2190', 'https://sun9-69.userapi.com/c205724/v205724613/e1932/_wkKjP4yMfI.jpg'),
-			 ('RED', '2190', 'https://sun9-60.userapi.com/c205724/v205724613/e1960/WbXWKZmHEAA.jpg'),
-			 ('BORDEAUX', '2190', 'https://sun9-27.userapi.com/c205724/v205724613/e1ab7/-IayljJ1_dk.jpg'),
-			 ('PINK/GRAY', '2190', 'https://sun9-13.userapi.com/c205724/v205724626/de2b1/qJBs6A45xgY.jpg'),
-			 ('GRAY/PINK', '2190', 'https://sun9-34.userapi.com/c205724/v205724626/de397/BmCZTlkLO2U.jpg'),
-			 ('GRAY', '2190', 'https://sun9-28.userapi.com/c205724/v205724626/de300/V_peOM7e_Wc.jpg'),
-			 ('GRAY/RED', '2190', 'https://sun9-71.userapi.com/c857216/v857216788/159a9b/e080byKqFEA.jpg')
-			  ],
-		'Hamburg':
-			[('PINK', '2190', 'https://sun9-20.userapi.com/c857024/v857024503/e7ee3/rCRboThFVUE.jpg'),
-			 ('GRAY', '2190', 'https://sun9-46.userapi.com/c857024/v857024503/e7ef2/PDva-q6Tsjc.jpg'),
-			 ('RED', '2190', 'https://sun9-46.userapi.com/c857024/v857024503/e7fae/YZgAhcsClCQ.jpg'),
-			 ('CLASSIC', '2190', 'https://sun9-56.userapi.com/c857024/v857024048/eafff/_r4YrAlJRP4.jpg'),
-			 ('SKY BLUE', '2190', 'https://sun9-6.userapi.com/c857024/v857024048/eb03d/RtTp1HmxNFs.jpg'),
-			 ('TRIPLE BLACK', '2190', 'https://sun9-10.userapi.com/c857024/v857024048/eb058/Z63BP919Ddc.jpg'),
-			 ('DARK BLUE', '2190', 'https://sun9-48.userapi.com/c857120/v857120626/15c9a8/eslzfWkk6Ws.jpg'),
-			 ('BORDEAUX', '2190', 'https://sun9-46.userapi.com/c205724/v205724626/de3bd/iY3Du2Iz-eo.jpg'),
-			 ('BLUE/RED', '2190', 'https://sun9-32.userapi.com/c205724/v205724626/de41e/r0PI-QwJv_c.jpg'),
-			 ('RED/BLACK', '2190', 'https://sun9-36.userapi.com/c855232/v855232788/21b2f4/ikNbguR4XKc.jpg')
-			 ],
-		'Spezial':
-			[('ORANGE/GRAY', '2490', 'https://sun9-2.userapi.com/c857024/v857024934/eb82f/6UjNemkENbg.jpg'),
-			 ('CLASSIC', '2490', 'https://sun9-66.userapi.com/c858528/v858528788/154309/9o_ps9AkGPU.jpg'),
-			 ('TRIPLE BLACK', '2490', 'https://sun9-21.userapi.com/c858528/v858528629/154145/yJIAjqBhM2Q.jpg')
-			],
-		'Samba':
-			[('WHITE/BLACK', '2490', 'https://sun9-41.userapi.com/c857024/v857024934/eb8b6/j9cCC6UZmD0.jpg'),
-			 ('BEIGE/RED', '2490', 'https://sun9-67.userapi.com/c857024/v857024934/eb8f1/M9ATpDCBuak.jpg')
-			],
-		'EQT':
-			[('Red Gray White', 'price', 'https://images.ua.prom.st/1215176577_w640_h640_adidas-eqt-cushion.jpg'),
-			 ('Black White', 'price', 'https://stockx.imgix.net/adidas-EQT-Support-93-17-Black-White-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&q=90&dpr=2&trim=color&updated_at=1543951666'),
-			 ('Green Black White', 'price', 'https://stockx.imgix.net/Adidas-EQT-Support-ADV-Core-Black-Green-Vintage.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&q=90&dpr=2&trim=color&updated_at=1538080256')
-			 ],
-		'ZX 8000':
-			[('Aqua', 'price', 'https://media.endclothing.com/media/f_auto,w_600,h_600/prodmedia/media/catalog/product/2/0/20-12-2019_adidas_zx8000og_aquayellow_eg8784_aj_1.jpg'),
-			 ('Aqua Orange', 'price', 'https://sun9-32.userapi.com/impf/c206528/v206528224/b5f54/k90IfmSHZEU.jpg?size=639x0&quality=90&sign=dee0f9e5c6aabafa567ed87daa36f58b'),
-			 ('Gray Green', 'price', 'https://www.atlanticsurvivalgear.ca/images/ca1/Adidas%20zx%208000%20schwarz%20grau%20gr%20n%20schuhe%20Online%20bestellen%201032.jpg')
-			 ],
-		'Yeezy 500':
-			[('Gray', 'price', 'https://yeezyboost.store/image/cache/catalog/foto/yeezy-boost-folder/yeezy500/dh1iiafvmaai8vm-1000x1340.jpg'),
-			 ('Beige', 'price', 'https://images.ua.prom.st/1261499077_w640_h2048_1256160480_w800_h640_1219__pid728608023_588aad1f.png?PIMAGE_ID=1261499077')
-			 ],
-		'Disruptor II':
-			[('White', 'price', 'https://i.ebayimg.com/images/g/IZcAAOSwWk1ds7kI/s-l640.jpg'),
-			 ('Black', 'price', 'https://imgs.inkfrog.com/pix/baybayshoes/Fila-Disruptor-Black-White-Red-1.jpg'),
-			 ('Winter White', 'price', 'https://images.ua.prom.st/1391205418_w640_h640_fila-disruptor-ii.jpg'),
-			 ('Winter Black', 'price', 'https://sneakers-kross.com.ua/content/images/41/fila-disruptor-ii-black-fur-winter-98163105654161_small11.jpg'),
-			 ('White Premium', 'price', 'https://imgs.inkfrog.com/pix/baybayshoes/Womens-Fila-Disruptor-2-Premium-White-White-1.jpg'),
-			 ('Black Premium', 'price', 'https://i.ebayimg.com/images/g/zfEAAOSwroJcdEUs/s-l300.jpg'),
-			 ('White Pink', 'price', 'https://cross-city.ru/upload/iblock/5e5/fila-disruptor-ii-white-pink_p139_1.jpg'),
-			 ('Black suede', 'price', 'https://images.ru.prom.st/728637820_w640_h640_muzhskie-krossovki-fila.jpg'),
-			 ],
-		'Sandals':
-			[('White', 'price', 'https://sun9-27.userapi.com/impf/c206828/v206828916/ac313/LiRz5LVpUDU.jpg?size=0x640&quality=90&sign=005ce9701b9320ca3500d79a66738bf7'),
-			 ('Black White', 'price', 'https://images.ua.prom.st/1197241433_w640_h640_fila-disruptor-sandals.jpg'),
-			 ('Pink Black', 'price', 'https://sun9-60.userapi.com/impf/c857016/v857016583/1263e7/g02xBJtkZv8.jpg?size=0x800&quality=90&sign=b86f58bedbb94c3a924db49041e1a58f'),
-			 ('White Red Blue', 'price', 'https://www.londonstore.it/img/8719477171992_P.jpg'),
-			 ('Black Gold', 'price', 'https://scamper.com.ua/content/images/44/bosonizhki-fila-disruptor-sandals-black-gold-59434697306608_small11.jpg'),
-			 ]
-	}
+#	Colors = {
+#		'Gazelle':
+#			[('PINK', '2190', 'https://sun9-67.userapi.com/c205724/v205724730/ded27/T1DCjpehweM.jpg'),
+#			 ('BLACK', '2190', 'https://sun9-9.userapi.com/c205724/v205724730/ded4e/yxSS1YH3Dkw.jpg'),
+#			 ('SKY BLUE', '2190', 'https://sun9-48.userapi.com/c205724/v205724613/e18d3/TKPTlBNQ39A.jpg'),
+#			 ('GRAY BLACK', '2190', 'https://sun9-20.userapi.com/c205724/v205724613/e18ca/lBEx0-ob0Vs.jpg'),
+#			 ('BLUE', '2190', 'https://sun9-20.userapi.com/c205724/v205724613/e1902/jwWux3r32YM.jpg'),
+#			 ('GREEN', '2190', 'https://sun9-69.userapi.com/c205724/v205724613/e1932/_wkKjP4yMfI.jpg'),
+#			 ('RED', '2190', 'https://sun9-60.userapi.com/c205724/v205724613/e1960/WbXWKZmHEAA.jpg'),
+#			 ('BORDEAUX', '2190', 'https://sun9-27.userapi.com/c205724/v205724613/e1ab7/-IayljJ1_dk.jpg'),
+#			 ('PINK/GRAY', '2190', 'https://sun9-13.userapi.com/c205724/v205724626/de2b1/qJBs6A45xgY.jpg'),
+#			 ('GRAY/PINK', '2190', 'https://sun9-34.userapi.com/c205724/v205724626/de397/BmCZTlkLO2U.jpg'),
+#			 ('GRAY', '2190', 'https://sun9-28.userapi.com/c205724/v205724626/de300/V_peOM7e_Wc.jpg'),
+#			 ('GRAY/RED', '2190', 'https://sun9-71.userapi.com/c857216/v857216788/159a9b/e080byKqFEA.jpg')
+#			  ],
+#		'Hamburg':
+#			[('PINK', '2190', 'https://sun9-20.userapi.com/c857024/v857024503/e7ee3/rCRboThFVUE.jpg'),
+#			 ('GRAY', '2190', 'https://sun9-46.userapi.com/c857024/v857024503/e7ef2/PDva-q6Tsjc.jpg'),
+#			 ('RED', '2190', 'https://sun9-46.userapi.com/c857024/v857024503/e7fae/YZgAhcsClCQ.jpg'),
+#			 ('CLASSIC', '2190', 'https://sun9-56.userapi.com/c857024/v857024048/eafff/_r4YrAlJRP4.jpg'),
+#			 ('SKY BLUE', '2190', 'https://sun9-6.userapi.com/c857024/v857024048/eb03d/RtTp1HmxNFs.jpg'),
+#			 ('TRIPLE BLACK', '2190', 'https://sun9-10.userapi.com/c857024/v857024048/eb058/Z63BP919Ddc.jpg'),
+#			 ('DARK BLUE', '2190', 'https://sun9-48.userapi.com/c857120/v857120626/15c9a8/eslzfWkk6Ws.jpg'),
+#			 ('BORDEAUX', '2190', 'https://sun9-46.userapi.com/c205724/v205724626/de3bd/iY3Du2Iz-eo.jpg'),
+#			 ('BLUE/RED', '2190', 'https://sun9-32.userapi.com/c205724/v205724626/de41e/r0PI-QwJv_c.jpg'),
+#			 ('RED/BLACK', '2190', 'https://sun9-36.userapi.com/c855232/v855232788/21b2f4/ikNbguR4XKc.jpg')
+#			 ],
+#		'Spezial':
+#			[('ORANGE/GRAY', '2490', 'https://sun9-2.userapi.com/c857024/v857024934/eb82f/6UjNemkENbg.jpg'),
+#			 ('CLASSIC', '2490', 'https://sun9-66.userapi.com/c858528/v858528788/154309/9o_ps9AkGPU.jpg'),
+#			 ('TRIPLE BLACK', '2490', 'https://sun9-21.userapi.com/c858528/v858528629/154145/yJIAjqBhM2Q.jpg')
+#			],
+#		'Samba':
+#			[('WHITE/BLACK', '2490', 'https://sun9-41.userapi.com/c857024/v857024934/eb8b6/j9cCC6UZmD0.jpg'),
+#			 ('BEIGE/RED', '2490', 'https://sun9-67.userapi.com/c857024/v857024934/eb8f1/M9ATpDCBuak.jpg')
+#			],
+#		'EQT':
+#			[('Red Gray White', 'price', 'https://images.ua.prom.st/1215176577_w640_h640_adidas-eqt-cushion.jpg'),
+#			 ('Black White', 'price', 'https://stockx.imgix.net/adidas-EQT-Support-93-17-Black-White-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&q=90&dpr=2&trim=color&updated_at=1543951666'),
+#			 ('Green Black White', 'price', 'https://stockx.imgix.net/Adidas-EQT-Support-ADV-Core-Black-Green-Vintage.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&q=90&dpr=2&trim=color&updated_at=1538080256')
+#			 ],
+#		'ZX 8000':
+#			[('Aqua', 'price', 'https://media.endclothing.com/media/f_auto,w_600,h_600/prodmedia/media/catalog/product/2/0/20-12-2019_adidas_zx8000og_aquayellow_eg8784_aj_1.jpg'),
+#			 ('Aqua Orange', 'price', 'https://sun9-32.userapi.com/impf/c206528/v206528224/b5f54/k90IfmSHZEU.jpg?size=639x0&quality=90&sign=dee0f9e5c6aabafa567ed87daa36f58b'),
+#			 ('Gray Green', 'price', 'https://www.atlanticsurvivalgear.ca/images/ca1/Adidas%20zx%208000%20schwarz%20grau%20gr%20n%20schuhe%20Online%20bestellen%201032.jpg')
+#			 ],
+#		'Yeezy 500':
+#			[('Gray', 'price', 'https://yeezyboost.store/image/cache/catalog/foto/yeezy-boost-folder/yeezy500/dh1iiafvmaai8vm-1000x1340.jpg'),
+#			 ('Beige', 'price', 'https://images.ua.prom.st/1261499077_w640_h2048_1256160480_w800_h640_1219__pid728608023_588aad1f.png?PIMAGE_ID=1261499077')
+#			 ],
+#		'Disruptor II':
+#			[('White', 'price', 'https://i.ebayimg.com/images/g/IZcAAOSwWk1ds7kI/s-l640.jpg'),
+#			 ('Black', 'price', 'https://imgs.inkfrog.com/pix/baybayshoes/Fila-Disruptor-Black-White-Red-1.jpg'),
+#			 ('Winter White', 'price', 'https://images.ua.prom.st/1391205418_w640_h640_fila-disruptor-ii.jpg'),
+#			 ('Winter Black', 'price', 'https://sneakers-kross.com.ua/content/images/41/fila-disruptor-ii-black-fur-winter-98163105654161_small11.jpg'),
+#			 ('White Premium', 'price', 'https://imgs.inkfrog.com/pix/baybayshoes/Womens-Fila-Disruptor-2-Premium-White-White-1.jpg'),
+#			 ('Black Premium', 'price', 'https://i.ebayimg.com/images/g/zfEAAOSwroJcdEUs/s-l300.jpg'),
+#			 ('White Pink', 'price', 'https://cross-city.ru/upload/iblock/5e5/fila-disruptor-ii-white-pink_p139_1.jpg'),
+#			 ('Black suede', 'price', 'https://images.ru.prom.st/728637820_w640_h640_muzhskie-krossovki-fila.jpg'),
+#			 ],
+#		'Sandals':
+#			[('White', 'price', 'https://sun9-27.userapi.com/impf/c206828/v206828916/ac313/LiRz5LVpUDU.jpg?size=0x640&quality=90&sign=005ce9701b9320ca3500d79a66738bf7'),
+#			 ('Black White', 'price', 'https://images.ua.prom.st/1197241433_w640_h640_fila-disruptor-sandals.jpg'),
+#			 ('Pink Black', 'price', 'https://sun9-60.userapi.com/impf/c857016/v857016583/1263e7/g02xBJtkZv8.jpg?size=0x800&quality=90&sign=b86f58bedbb94c3a924db49041e1a58f'),
+#			 ('White Red Blue', 'price', 'https://www.londonstore.it/img/8719477171992_P.jpg'),
+#			 ('Black Gold', 'price', 'https://scamper.com.ua/content/images/44/bosonizhki-fila-disruptor-sandals-black-gold-59434697306608_small11.jpg'),
+#			 ]
+#	}
